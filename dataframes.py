1885dcf64c2028f954421b4e66131d6cdd69f2f1
fuentestomas <tomas.e.fuentes@gmail.com>
2022-11-13 21:44:43 -0300

    agrego comentarios

--- dataframes.py
+++ dataframes.py
@@ -2,8 +2,11 @@ import pandas as pd
 from sqlalchemy import create_engine
 
 def createDataFrame():
+    # Definimos la conexion con la base de datos
     connection = create_engine('mysql+mysqldb://sql10553695:gJkK8ArDCg@sql10.freesqldatabase.com/sql10553695')
     '''
+    Datos correspondientes a la conexion
+
     User=sql10553695
     Password=gJkK8ArDCg
     Database=sql10553695
@@ -12,10 +15,14 @@ def createDataFrame():
     '''
     query = 'SELECT * FROM pagos'
 
+    # Generamos DataFrames con la informacion de cada una de las fuentes
     payments = pd.read_sql(query, connection)
     client_data = pd.read_json('client_data.json')
     metadata = pd.read_csv('metadata.csv')
 
+    # Combinamos los DataFrames para tener un solo dataset. Combina primero la informacion de los clientes
+    # con su metadata y luego combina el resultado con los pagos correspondientes
     mergedf = client_data.merge(metadata, on=['id']).merge(payments, left_on=['id'], right_on=['client_id'])
 
+    # Devolvemos el DataFrame obtenido
     return mergedf
